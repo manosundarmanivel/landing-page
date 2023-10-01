4aa6f3419ed4d8351caa86be8383d675e50ceca5
manosundarmanivel <manosundarmanivel@gmail.com>
2023-10-01 14:21:07 +0530

    code update on routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import HomePic from "../src/Assets/img/home.svg";
 
-function App() {
+import Footer from "../src/Components/Footer";
+
+  
+  
+
+
+const Home = () => {
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='font-poppins '>
+      <div className="flex justify-between items-center px-20 py-3 ">
+        <h1 className="text-[30px] font-extrabold">Profilio<span className="text-[#f6a130]">.in</span></h1>
+        <div className="border  border-[#323234] p-3">
+          <h1>Try for free</h1>
+        </div>
+      </div>
+      <div className=" justify-between items-center   flex bg-[#fdebcf] px-20 py-20">
+        <div className="">
+          <h1 className="font-bold text-centre text-[60px] text-[#323234] ">
+            Showcase Your Code <br />
+            Journey, All in one place !
+          </h1>
+          <h1 className="font-bold text-[30px] mt-5 text-[#323234]">
+            We Unite your Coding Presence.
+          </h1>
+          <div className="m-5">
+          <button onClick={()=>{
+           
+          }} className="p-3 mt-12 px-12 mt-3 mx-2 rounded-lg bg-[#323234]  text-white text-[20px]">
+            Comming Soon
+          </button>
+          
+          </div>
+          
+        </div>
+        <div className="flex justify-center ">
+        <img alt="img" src={HomePic} className="w-[400px] " />
+        </div>
+      </div>
+    
+      <Footer/>
     </div>
   );
-}
+};
 
-export default App;
+export default Home;
---
+++ src/Components/Footer.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="flex justify-around bg-[#f6a130] py-14">
+        <div>
+          <h1 className="text-[20px] font-extrabold">Profolio.in</h1>
+          <h1 className="py-2">Showcase Your Code <br />
+            Journey</h1>
+          <h1>Stay in touch with us:</h1>
+          <div className="flex">
+
+          </div>
+        </div>
+        <div className="flex ">
+          <div className="px-10">
+            <h1 className="">Company</h1>
+            <h1>About</h1>
+            <h1>Careers</h1>
+            <h1>Training</h1>
+          </div>
+          <div className="px-10">
+            <h1>Services</h1>
+            <h1>Designing</h1>
+            <h1>Development</h1>
+            <h1>Security</h1>
+          </div>
+          <div className="px-10 f">
+            <h1>Quick Links</h1>
+            <h1>Home</h1>
+            <h1>Protfolio</h1>
+            <h1>Producs</h1>
+          </div>
+        </div>
+      </div>
+  )
+}
+
+export default Footer
